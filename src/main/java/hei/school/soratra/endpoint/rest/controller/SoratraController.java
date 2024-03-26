b0fb3f6a699310b978ed3ce522e9370162d329bc
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-03-26 06:28:40 +0100

    fist commit

---
+++ src/main/java/hei/school/soratra/endpoint/rest/controller/SoratraController.java
@@ -0,0 +1,11 @@
+package hei.school.soratra.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class SoratraController {
+    @PutMapping()
+}
